4e4e59f2e8252e2949a857b8f5db6453e70df929
selt1 <jon.g.kuhl@gmail.com>
2017-11-13 17:11:51 +0000

    updated 11/13/17

--- spec/javascripts/rottenpotatoes_spec.js
+++ spec/javascripts/rottenpotatoes_spec.js
@@ -1,5 +1,6 @@
 //=require jquery
 //=require jasmine-jquery
+
 describe('MoviePopup', function() {
   describe('setup', function() {
     it('adds popup Div to main page', function() {
@@ -10,7 +11,25 @@ describe('MoviePopup', function() {
     });
   });
   describe('clicking on movie link', function() {
-//    beforeEach(function() { loadFixtures('movie_row.html'); });
+  // Can't get fixture loading to work.  Must be a path problem  
+ //  beforeEach(function() {loadFixtures('movie_row.html')})
+ // The fixture is set after page load.  Hence Javascript Setup
+ // has already run.  SO the handler will not be properly attached
+ //to the a element.
+   beforeEach(function() {setFixtures(
+      '<table id="movies">'+
+       '<tbody>'+
+         '<tr>'+
+           '<td>Aladdin</td>'+
+           '<td>G</td>'+
+           '<td><a href="/movies/1">More about Alladin</a></td>'+
+         '</tr>'+
+       '</tbody>'+
+      '</table>')
+    });
+ /*   
+ //  Notice that if we explicitly add javascript to the fixture
+ // to attach the handler,the tests will pass
     beforeEach(function() {setFixtures(
       '<table id="movies">'+
        '<tbody>'+
@@ -19,16 +38,21 @@ describe('MoviePopup', function() {
            '<td>G</td>'+
            '<td><a href="/movies/1">More about Alladin</a></td>'+
          '</tr>'+
-      '</tbody>'+
-     '</table>')});
+       '</tbody>'+
+      '</table>'+
+     '<script>'+
+        "$('#movies a').click(RP.getMovieInfo)"+
+     '</script>')
+    });
+ */   
     it('calls correct URL', function() {
       spyOn($, 'ajax');
       $('#movies a').trigger('click');
-      expect($.ajax.calls.mostRecent().args[0]['url']).toEqual('/movies/1');
+     expect($.ajax.calls.mostRecent().args[0]['url']).toEqual('/movies/1');
     });
+    
     describe('when successful server call', function() {
       beforeEach(function() {
-  //      var jsonResponse = getJSONFixture('movie_info.json');
         var jsonResponse= {"id":1,"title":"Aladdin","rating":"G",
                           "description":null,"release_date":"1992-11-25T00:00:00.000Z",
                           "created_at":"2016-10-28T16:03:45.429Z",
